ab650fe9310ae896b8195d5335013ec42076cb0d
rafayet <78610949+rafayet-git@users.noreply.github.com>
2024-11-03 22:20:35 -0500

    lab1

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Timetabled</h1>
+      <h2>Is this your Scheduele</h2>
+      <Calendar />
+
+    </div>
+  )
+}
+
+export default App;
